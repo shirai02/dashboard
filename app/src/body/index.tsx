2a7ebc35230801ee34ad4682d6ff9dc88ed20d15
desktop <kazuking.1911@gmail.com>
2020-11-29 17:56:13 +0900

    body追加

---
+++ app/src/body/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const body: React.FC = () => {
+    return (
+        <React.Fragment>
+            <div>ここに書いていく</div>
+        </React.Fragment>
+    );
+}
+
+export default body;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Body from './body';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Body />
   </React.StrictMode>,
   document.getElementById('root')
 );
